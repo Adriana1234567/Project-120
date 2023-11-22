6a9aa3d4d7da75a3a7d25a1d9b2ad63833a24f40
Adriana1234567 <111986480+Adriana1234567@users.noreply.github.com>
2023-11-22 17:37:14 +0000

    Add files via upload

---
+++ Project 102/Move_file.py
@@ -0,0 +1,19 @@
+import os 
+import shutil
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for i in list_of_files:
+    
+
+
+
+if os.path.exists(path2):
+    print("Moving"+file_name+"...")
+
+    shutil.move(path1,path3)
+else:
+    os.makedirs(path2)
+    print("Moving"+file_name+"...")
+    shutil.move(path1,path3)
